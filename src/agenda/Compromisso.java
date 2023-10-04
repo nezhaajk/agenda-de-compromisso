85fcef2f74980ee449c59ff3d214584900da4657
henrique <henrique.l19@aluno.ifsc.edu.br>
2023-10-04 14:41:26 -0300

    Teste

--- src/agenda/Compromisso.java
+++ src/agenda/Compromisso.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 
 public class Compromisso {
 
-			public Compromisso(String nome, String hora, String data, String local, String descricao) {
+	public Compromisso(String nome, String hora, String data, String local, String descricao) {
 
-			}
+	}
 
-			public static void main(String[] args) {
-			Integer opSelecionada = Integer.MAX_VALUE;
-			Scanner sc = new Scanner(System.in);
-			ArrayList<Compromisso> compromissos = new ArrayList<>();
+	public static void main(String[] args) {
 
-			while (opSelecionada != 0) {
+		// git é magico
+		
+		Integer opSelecionada = Integer.MAX_VALUE;
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Compromisso> compromissos = new ArrayList<>();
+
+		while (opSelecionada != 0) {
 
 			System.out.println("- MENU -");
 			System.out.println("1. CADASTRAR");
@@ -28,65 +31,64 @@ public class Compromisso {
 			switch (opSelecionada) {
 
 			case 1: {
-			System.out.println("Digite o nome do compromisso:");
-			String nome = sc.nextLine();
-			System.out.println("Digite a hora do compromisso:");
-			String hora = sc.nextLine();
-			float Hora = Float.valueOf(hora);
-			System.out.println("Digite a data do compromisso: ");//como fazer data?
-			String data = sc.nextLine();
-			// conversao de tipo
-			float Data = Float.valueOf(data);
-			System.out.println("Digite a localizaçao do compromisso: ");
-			String local = sc.nextLine();
-			System.out.println("Digite a descricao do compromisso: ");
-			String descricao = sc.nextLine();
+				System.out.println("Digite o nome do compromisso:");
+				String nome = sc.nextLine();
+				System.out.println("Digite a hora do compromisso:");
+				String hora = sc.nextLine();
+				float Hora = Float.valueOf(hora);
+				System.out.println("Digite a data do compromisso: ");// como fazer data?
+				String data = sc.nextLine();
+				// conversao de tipo
+				float Data = Float.valueOf(data);
+				System.out.println("Digite a localizaçao do compromisso: ");
+				String local = sc.nextLine();
+				System.out.println("Digite a descricao do compromisso: ");
+				String descricao = sc.nextLine();
 
-			Compromisso c = new Compromisso(nome, hora, data, local, descricao);
-			compromissos.add(c);
-			break;
+				Compromisso c = new Compromisso(nome, hora, data, local, descricao);
+				compromissos.add(c);
+				break;
 
 			}
 
 			case 2: {
-			System.out.println("Digite o ID para fazer a edicao:");
+				System.out.println("Digite o ID para fazer a edicao:");
 
-			// para verificar se existe precisa de um if para receber novas informaçoes
-			// finalizar codigo
-			break;
+				// para verificar se existe precisa de um if para receber novas informaçoes
+				// finalizar codigo
+				break;
 			}
 			case 3: {
-			System.out.println("Digite o ID para fazer a exclusao:");
-			// precisa de um if para verificar se existe?
-			// confirmar a exclusão e remover do armazenamento.
-			// finalizar codigo
-			break;
+				System.out.println("Digite o ID para fazer a exclusao:");
+				// precisa de um if para verificar se existe?
+				// confirmar a exclusão e remover do armazenamento.
+				// finalizar codigo
+				break;
 			}
 			case 4: {
-			// Saida de dados (output)
-			System.out.println("Lista dos titulos dos compromissos: ");
-			for (Compromisso compromisso : compromissos) {
-			System.out.println("Nome: " + compromisso.getNome());
-			break;
-			}
+				// Saida de dados (output)
+				System.out.println("Lista dos titulos dos compromissos: ");
+				for (Compromisso compromisso : compromissos) {
+					System.out.println("Nome: " + compromisso.getNome());
+					break;
+				}
 			}
 			case 5: {
-			System.out.println("Programa Encerrado!");
-			break;
+				System.out.println("Programa Encerrado!");
+				break;
 			}
 			default: {
-			System.out.println("Opcao invalida!Tente novamente");
+				System.out.println("Opcao invalida!Tente novamente");
 			}
 			}
-			}
-
-			sc.close();
-
-			}
-
-			private String getNome() {
-			return getNome();
-			}
-
 		}
-	
+
+		sc.close();
+
+	}
+
+	private String getNome() {
+		return getNome();
+	}
+
+}
